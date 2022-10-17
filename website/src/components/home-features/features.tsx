24048fdeec283794ecae7b03a0589d42c7d49f29
Andrei Pampu <pampu.s.andrei@gmail.com>
2022-10-17 23:23:38 +0300

    docs: update fix static pages

--- website/src/components/home-features/features.tsx
+++ website/src/components/home-features/features.tsx
@@ -37,7 +37,7 @@ const FeatureList: FeatureProps[] = [
   },
 ];
 
-export function Features() {
+export function HomeFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,16 +1,16 @@
 import Layout from '@theme/Layout';
 import React from 'react';
-import { Features } from './home/features';
-import { Header } from './home/header';
-import { Snippets } from './home/snippets/snippets';
+import { HomeFeatures } from '../components/home-features';
+import { HomeHeader } from '../components/home-header';
+import { HomeSnippets } from '../components/home-snippets';
 
 export default function Home(): JSX.Element {
   return (
     <Layout title={`Home`} description="Schema first without the hassle">
-      <Header />
+      <HomeHeader />
       <main>
-        <Features />
-        <Snippets />
+        <HomeFeatures />
+        <HomeSnippets />
       </main>
     </Layout>
   );

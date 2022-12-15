f24f4b31b0dc18e713a47c8a68160a8dfe418de1
Andrei Pampu <pampu.s.andrei@gmail.com>
2022-12-15 15:38:50 +0200

    fix: enable code splitting for cjs

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@baeta/prep';
 export default defineConfig({
   entry: ['index.ts', 'sdk/index.ts'],
   format: ['esm', 'cjs'],
-  external: ['@baeta/compiler'],
   dts: true,
   clean: true,
+  splitting: true,
 });
--- packages/generator/tsup.config.ts
+++ packages/generator/tsup.config.ts
@@ -5,4 +5,5 @@ export default defineConfig({
   format: ['esm', 'cjs'],
   dts: true,
   clean: true,
+  splitting: true,
 });

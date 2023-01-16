ad215b915867aa28b67c42c9dc1412fc38c0013d
Andrei Pampu <pampu.s.andrei@gmail.com>
2023-01-16 21:44:04 +0200

    chore: remove test files

--- packages/cli/baeta.js
+++
@@ -1,7 +0,0 @@
-import { createConfig } from './dist/index.js';
-
-export default createConfig({
-  a: 4,
-});
-
-
--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from '@baeta/build-tools/tsup';
 export default defineConfig({
   entry: ['./bin/cli.ts', './index.ts'],
   format: ['esm', 'cjs'],
+  dts: true,
   clean: true,
   bundle: true,
-  external: [],
-  noExternal: ['@baeta/config', '@baeta/generator', "@baeta/plugin-graphql", "@baeta/plugin"],
 });

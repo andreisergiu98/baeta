6d03f5b3c38bb6d10485507d1b2dc5d7d45204e5
Andrei Pampu <pampu.s.andrei@gmail.com>
2023-02-10 22:50:50 +0200

    feat(core): add context store utility

--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -1,5 +1,7 @@
 export { createApplication } from './application';
 export type { Options } from './application';
+export { createContextStore } from './ctx-store';
+export type { ContextStoreOptions } from './ctx-store';
 export { createExtensions } from './extensions';
 export { createInputDirective } from './input-directive';
 export type { ValidationDirectiveFn, ValidationTarget } from './input-directive';

b4c5a71f8a7d0191353b8e6c1decb5cf177d7c13
Andrei Pampu <pampu.s.andrei@gmail.com>
2023-05-07 00:15:24 +0300

    feat(generator): improve watcher ignore

--- packages/generator-sdk/lib/fs-watcher.ts
+++ packages/generator-sdk/lib/fs-watcher.ts
@@ -1,9 +1,9 @@
 export { isMatch } from 'micromatch';
 export { micromatch };
 
-import micromatch from 'micromatch';
+import micromatch, { scan } from 'micromatch';
 
-import { AsyncSubscription, EventType, Options, subscribe } from '@parcel/watcher';
+import { AsyncSubscription, Event, EventType, Options, subscribe } from '@parcel/watcher';
 import path from 'path';
 
 export type WatcherListener = (path: WatcherFile) => void;
@@ -17,6 +17,74 @@ export interface WatcherFile {
   relativePath: string;
 }
 
+class WatcherIgnore {
+  private ignoredFiles: string[] = [];
+  private ignoredRegexes: RegExp[] = [];
+  private ignoredFunctions: MatchFn[] = [];
+  private ignoredGlobs: MatchFn[] = [];
+
+  private ignoredGlobsMap: Record<string, MatchFn | undefined> = {};
+
+  ignore(pattern: MatchPattern) {
+    if (typeof pattern !== 'string') {
+      if (typeof pattern === 'function') {
+        this.ignoredFunctions.push(pattern);
+      } else {
+        this.ignoredRegexes.push(pattern);
+      }
+      return;
+    }
+
+    const result = scan(pattern);
+
+    if (!result.isBrace && !result.isGlobstar && !result.isExtglob && !result.isGlob) {
+      this.ignoredFiles.push(pattern);
+    }
+
+    this.ignoredGlobsMap[pattern] = micromatch.matcher(pattern);
+    this.ignoredGlobs = Object.values(this.ignoredGlobsMap).filter(Boolean) as MatchFn[];
+  }
+
+  unignore(pattern: MatchPattern) {
+    if (typeof pattern === 'string') {
+      this.ignoredFiles = this.ignoredFiles.filter((p) => p !== pattern);
+
+      if (this.ignoredGlobsMap[pattern]) {
+        delete this.ignoredGlobsMap[pattern];
+        this.ignoredGlobs = Object.values(this.ignoredGlobsMap).filter(Boolean) as MatchFn[];
+      }
+    }
+
+    if (pattern instanceof RegExp) {
+      this.ignoredRegexes = this.ignoredRegexes.filter((p) => p !== pattern);
+    }
+
+    if (typeof pattern === 'function') {
+      this.ignoredFunctions = this.ignoredFunctions.filter((p) => p !== pattern);
+    }
+  }
+
+  isIgnored(path: string) {
+    if (this.ignoredFiles.includes(path)) {
+      return true;
+    }
+
+    if (this.ignoredGlobs.some((f) => f(path))) {
+      return true;
+    }
+
+    if (this.ignoredRegexes.some((r) => r.test(path))) {
+      return true;
+    }
+
+    if (this.ignoredFunctions.some((f) => f(path))) {
+      return true;
+    }
+
+    return false;
+  }
+}
+
 export class Watcher {
   private subscriptionPromise: Promise<AsyncSubscription>;
   private subscription: AsyncSubscription | undefined;
@@ -27,38 +95,35 @@ export class Watcher {
     delete: [],
   };
 
-  private ignored: MatchPattern[] = [];
+  private watcherIgnore = new WatcherIgnore();
 
   constructor(private readonly cwd: string, options?: Options) {
-    this.subscriptionPromise = subscribe(
-      cwd,
-      (err, events) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-
-        const filteredEvents = events.filter((event) => {
-          return !this.isIgnored(event.path);
-        });
-
-        for (const event of filteredEvents) {
-          for (const listener of this.listeners[event.type]) {
-            listener({
-              type: event.type,
-              path: event.path,
-              relativePath: path.relative(cwd, event.path),
-            });
-          }
-        }
-      },
-      options
-    );
-
+    this.subscriptionPromise = subscribe(cwd, this.onEvents, options);
     this.subscriptionPromise.then((subscription) => {
       this.subscription = subscription;
     });
   }
+
+  onEvents = (err: Error | null, events: Event[]) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const filteredEvents = events.filter((event) => {
+      return !this.watcherIgnore.isIgnored(event.path);
+    });
+
+    for (const event of filteredEvents) {
+      for (const listener of this.listeners[event.type]) {
+        listener({
+          type: event.type,
+          path: event.path,
+          relativePath: path.relative(this.cwd, event.path),
+        });
+      }
+    }
+  };
 
   on(event: EventType, listener: WatcherListener) {
     this.listeners[event].push(listener);
@@ -69,23 +134,11 @@ export class Watcher {
   }
 
   ignore(pattern: MatchPattern) {
-    this.ignored.push(pattern);
+    this.watcherIgnore.ignore(pattern);
   }
 
   unignore(pattern: MatchPattern) {
-    this.ignored = this.ignored.filter((p) => p !== pattern);
-  }
-
-  isIgnored(path: string) {
-    return this.ignored.some((pattern) => {
-      if (typeof pattern === 'string') {
-        return path === pattern || micromatch.isMatch(path, pattern);
-      } else if (pattern instanceof RegExp) {
-        return pattern.test(path);
-      } else {
-        return pattern(path);
-      }
-    });
+    this.watcherIgnore.unignore(pattern);
   }
 
   async close() {

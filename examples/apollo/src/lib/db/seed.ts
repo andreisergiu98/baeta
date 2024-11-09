658fd4174e367979c0c59dc864d22896d82e04fc
Andrei Pampu <pampu.s.andrei@gmail.com>
2024-11-09 03:07:38 +0200

    add cache examples

---
+++ examples/apollo/src/lib/db/seed.ts
@@ -0,0 +1,77 @@
+import { faker } from '@faker-js/faker';
+import { type Prisma, PrismaClient } from './prisma.ts';
+
+const prisma = new PrismaClient();
+
+function generateUser() {
+	const person: Prisma.UserCreateInput = {
+		email: faker.internet.email(),
+		lastName: faker.person.lastName(),
+		givenName: faker.person.firstName(),
+		birthday: faker.datatype.boolean() ? faker.date.past({ years: 20 }) : undefined,
+		phoneNumber: faker.datatype.boolean() ? faker.phone.number() : undefined,
+		profile: faker.datatype.boolean() ? faker.image.avatar() : undefined,
+	};
+
+	return person;
+}
+
+function generateUsers(len = 10) {
+	const users = [];
+	for (let i = 0; i < len; i++) {
+		users.push(generateUser());
+	}
+	return users;
+}
+
+function generatePhoto() {
+	const photo: Prisma.UserPhotoCreateWithoutUserInput = {
+		url: faker.image.url(),
+	};
+	return photo;
+}
+
+function generatePhotos(len = 10) {
+	const photos = [];
+	for (let i = 0; i < len; i++) {
+		photos.push(generatePhoto());
+	}
+	return photos;
+}
+
+async function insertUser(
+	user: Prisma.UserCreateInput,
+	photos: Prisma.UserPhotoCreateWithoutUserInput[],
+) {
+	return prisma.user.create({
+		data: {
+			...user,
+			photos: {
+				createMany: {
+					data: photos,
+				},
+			},
+		},
+	});
+}
+
+async function main() {
+	const users = generateUsers(faker.number.int({ min: 8, max: 15 }));
+
+	const promises = users.map(async (user) => {
+		const photos = generatePhotos(faker.number.int({ min: 7, max: 15 }));
+		return insertUser(user, photos);
+	});
+
+	return Promise.all(promises);
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
---
+++ examples/apollo/src/modules/user-photos/user-photos-pagination.resolvers.ts
@@ -0,0 +1,73 @@
+import { CacheRef } from '@baeta/extension-cache';
+import { ne } from '@faker-js/faker';
+import type * as Types from '../../__generated__/types.ts';
+import { db } from '../../lib/db/prisma.ts';
+import { createEdges } from '../../utils/pagination.ts';
+import { getUserPhotosModule } from './typedef.ts';
+import { userPhotoCache } from './user-photos-cache.ts';
+
+const { User } = getUserPhotosModule();
+
+export function customPhotosRef() {
+	return new CacheRef<Types.UserPhoto[], Types.User, Types.UserPhotosConnectionArgs>(
+		'User',
+		'photosConnection-custom',
+		User.photosConnection.$cacheRef.getHash(),
+	);
+}
+
+User.photosConnection(async ({ args, root }) => {
+	const photos = await db.user
+		.findUnique({
+			where: {
+				id: root.id,
+			},
+		})
+		.photos({
+			take: args.page.limit + 2,
+			skip: 0,
+			cursor: args.page.cursor
+				? {
+						id: args.page.cursor,
+					}
+				: undefined,
+		});
+
+	userPhotoCache.saveQueryResult(customPhotosRef(), photos, {
+		args,
+		parentRef: root.id,
+	});
+
+	return createEdges(photos ?? [], args.page.cursor, args.page.limit, (photo) => photo.id);
+});
+
+User.photosConnection.$use(async (params, next) => {
+	const cached = await userPhotoCache.getQueryResult(customPhotosRef(), {
+		args: params.args,
+		parentRef: params.root.id,
+	});
+
+	if (cached) {
+		return createEdges(
+			cached.query,
+			params.args.page.cursor,
+			params.args.page.limit,
+			(photo) => photo.id,
+		);
+	}
+
+	return next();
+});
+
+User.photosConnection.$auth((params) => {
+	if (params.args.page.limit <= 10) {
+		return {
+			$granted: 'readUserPhotos',
+		};
+	}
+
+	return {
+		$granted: 'readUserPhotos',
+		hasAccess: 'admin',
+	};
+});

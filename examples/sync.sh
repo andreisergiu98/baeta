001e93d79bcfba20249e7fadb3dc62aa3439ce05
Andrei Pampu <pampu.s.andrei@gmail.com>
2024-11-10 01:41:11 +0200

    patch cloudflare example in place

--- examples/sync.sh
+++ examples/sync.sh
@@ -9,5 +9,11 @@ cp -r ./examples/yoga-ws/src/__generated__ ./examples/apollo-ws/src
 cp -r ./examples/yoga/src/modules ./examples/cloudflare/src
 cp -r ./examples/yoga/src/__generated__ ./examples/cloudflare/src
 
-# cp -r ./examples/yoga-ws/src/modules ./examples/cloudflare-ws/src
+cp -r ./examples/yoga-ws/src/modules ./examples/cloudflare-ws/src
 cp -r ./examples/yoga-ws/src/__generated__ ./examples/cloudflare-ws/src
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' -e 's/ctx\.pubsub\.publish/ctx.publish/g' -e 's/ctx\.pubsub\.asyncIterator/ctx.subscribe/g' ./examples/cloudflare-ws/src/modules/user/user.resolvers.ts
+else
+    sed -i -e 's/ctx\.pubsub\.publish/ctx.publish/g' -e 's/ctx\.pubsub\.asyncIterator/ctx.subscribe/g' ./examples/cloudflare-ws/src/modules/user/user.resolvers.ts
+fi

24bec556aac171db3bdb52994003e5df619cca97
Andrei Pampu <pampu.s.andrei@gmail.com>
2024-11-10 01:30:21 +0200

    update generated files

--- examples/prisma/src/modules/user/typedef.ts
+++ examples/prisma/src/modules/user/typedef.ts
@@ -4,7 +4,7 @@
 import * as Types from "../../__generated__/types";
 import type { DocumentNode } from "graphql";
 import * as Baeta from "@baeta/core/sdk";
-import baetaExtensions from "../../extensions";
+import baetaExtensions from "../../extensions/index.ts";
 
 
 interface DefinedFields {

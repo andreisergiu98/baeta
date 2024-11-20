41012ff61533aa94335f4301311fd293d1e66aea
Andrei Pampu <pampu.s.andrei@gmail.com>
2024-11-20 15:07:59 +0200

    remove test config property

--- examples/scalars/baeta.ts
+++ examples/scalars/baeta.ts
@@ -4,7 +4,6 @@ import { autoloadPlugin } from '@baeta/plugin-autoload';
 export default defineConfig({
 	graphql: {
 		schemas: ['src/**/*.gql'],
-		baseTypesPath: 'src/__generated__/gen/base-types.ts',
 		scalars: {
 			DateTime: 'Date',
 			UUID: 'src/types/scalars.ts#UUID',

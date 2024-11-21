147e1b716181f6bf06da94e66adf73c475403041
Woky <pampu.andrei.social+git@pm.me>
2024-11-21 04:06:40 +0200

    chore: remove circular type imports (#171)
    * chore: remove some circular deps
    
    * fix: install madge
    
    * chore: remove some circular deps
    
    * rename file

--- packages/core/lib/input-directive/index.ts
+++ packages/core/lib/input-directive/index.ts
@@ -1,2 +1,3 @@
 export * from './input-directive.ts';
+export type { ValidationTarget } from './input-extensions.ts';
 export { addValidationToSchema } from './input-schema.ts';
---
+++ packages/core/sdk/resolver-maps.ts
@@ -0,0 +1,20 @@
+import type { ScalarResolver } from '../lib/scalar.ts';
+import type { NativeMiddleware } from './middleware.ts';
+import type { NativeTypeResolver } from './resolver-type.ts';
+import type { NativeResolver } from './resolver.ts';
+import type { NativeSubscription } from './subscription.ts';
+
+export type FieldResolvers = Record<string, NativeResolver> & {
+	__resolveType?: NativeTypeResolver;
+};
+
+export type SubscriptionsResolvers = {
+	Subscription?: Record<string, NativeSubscription | undefined>;
+};
+
+export type ResolversMap = {
+	[type: string]: FieldResolvers | undefined;
+} & SubscriptionsResolvers;
+
+export type ScalarsMap = Record<string, ScalarResolver | undefined>;
+export type MiddlewareMap = Record<string, NativeMiddleware[] | undefined>;

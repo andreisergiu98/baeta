55c67d8c484caf2390474662107ab510ecdccacf
Andrei Pampu <pampu.s.andrei@gmail.com>
2024-11-24 14:15:00 +0200

    add sitemaps

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -49,6 +49,18 @@ const config = {
 						require.resolve('./style/utilities.css'),
 					],
 				},
+				sitemap: {
+					lastmod: 'date',
+					changefreq: 'weekly',
+					priority: 0.5,
+					ignorePatterns: ['/tags/**'],
+					filename: 'sitemap.xml',
+					createSitemapItems: async (params) => {
+						const { defaultCreateSitemapItems, ...rest } = params;
+						const items = await defaultCreateSitemapItems(rest);
+						return items.filter((item) => !item.url.includes('/page/'));
+					},
+				},
 			}),
 		],
 	],

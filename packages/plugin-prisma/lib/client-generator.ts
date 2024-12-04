b9a149369cb6b2112aefc57eb40141239cc7c92c
Andrei Pampu <pampu.s.andrei@gmail.com>
2024-12-04 21:58:07 +0200

    fix prisma plugin skip fn

--- packages/plugin-prisma/lib/client-generator.ts
+++ packages/plugin-prisma/lib/client-generator.ts
@@ -9,25 +9,11 @@ async function compareSchemas(cwd: string, current: string, generated: string) {
 		readFile(resolve(cwd, current), 'utf-8'),
 		readFile(resolve(cwd, generated), 'utf-8').catch(() => null),
 	]);
-	return currentSchema === generatedSchema;
+	return currentSchema.replaceAll(' ', '') === generatedSchema?.replaceAll(' ', '');
 }
 
 export function createPrismaClientPlugin(options: PrismaPluginOptions) {
 	const { prismaSchema, generateCommand, generatedSchemaPath } = options;
-
-	const skip = async (ctx: Ctx) => {
-		const schema = resolve(ctx.generatorOptions.cwd, prismaSchema);
-
-		if (ctx.watching && ctx.changedFile?.path !== schema) {
-			return true;
-		}
-
-		if (!ctx.watching && generatedSchemaPath) {
-			return compareSchemas(ctx.generatorOptions.cwd, prismaSchema, generatedSchemaPath);
-		}
-
-		return false;
-	};
 
 	return createExecPlugin({
 		name: 'prisma-client',
@@ -45,6 +31,18 @@ export function createPrismaClientPlugin(options: PrismaPluginOptions) {
 			watcher.on('update', handleChange);
 			watcher.on('delete', handleChange);
 		},
-		skip,
+		skip: async (ctx: Ctx) => {
+			const schema = resolve(ctx.generatorOptions.cwd, prismaSchema);
+
+			if (ctx.watching && ctx.changedFile?.path !== schema) {
+				return true;
+			}
+
+			if (!ctx.watching && generatedSchemaPath) {
+				return compareSchemas(ctx.generatorOptions.cwd, prismaSchema, generatedSchemaPath);
+			}
+
+			return false;
+		},
 	});
 }

c0dfe764eed0b392c55afc94e9ff51c96fe17904
Andrei Pampu <pampu.andrei@pm.me>
2025-11-28 13:06:04 +0200

    use js-base64 instead of implementing encoding

--- packages/util-encoding/lib/base64-url.ts
+++ packages/util-encoding/lib/base64-url.ts
@@ -1,24 +1,9 @@
-import {
-	hasNodeBase64Requirements,
-	nodeDecodeBase64Url,
-	nodeEncodeBase64Url,
-} from './base64-node.ts';
-import { nativeDecodeBase64Url, nativeEncodeBase64Url } from './base64-url-native.ts';
+import { Base64 } from 'js-base64';
 
-function createBase64UrlEncoder() {
-	const global = globalThis;
-
-	if (hasNodeBase64Requirements(global)) {
-		return {
-			encodeBase64Url: (value: string): string => nodeEncodeBase64Url(global, value),
-			decodeBase64Url: (value: string): string => nodeDecodeBase64Url(global, value),
-		};
-	}
-
-	return {
-		encodeBase64Url: (value: string): string => nativeEncodeBase64Url(value),
-		decodeBase64Url: (value: string): string => nativeDecodeBase64Url(value),
-	};
+export function encodeBase64Url(value: string) {
+	return Base64.encode(value, true);
 }
 
-export const { encodeBase64Url, decodeBase64Url } = createBase64UrlEncoder();
+export function decodeBase64Url(value: string) {
+	return Base64.decode(value);
+}

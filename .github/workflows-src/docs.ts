c2d7be6e0bb221200843f9a1da02ba2b5af066be
Andrei Pampu <pampu.andrei@pm.me>
2026-05-03 20:35:16 +0300

    ci: build website without deploy on PR

--- .github/workflows-src/docs.ts
+++ .github/workflows-src/docs.ts
@@ -1,11 +1,11 @@
-import createWorkflow from 'github-actions-workflow-builder';
+import createWorkflow, { type Steps } from 'github-actions-workflow-builder';
 import { github, secrets } from 'github-actions-workflow-builder/context';
 import { interpolate } from 'github-actions-workflow-builder/lib/expression';
 import { setupNode } from './_shared/setup.ts';
 
 export default createWorkflow(
 	({ setWorkflowName, setPermissions, setConcurrency, addTrigger, addJob }) => {
-		setWorkflowName('Deploy website');
+		setWorkflowName('Build website');
 		setPermissions({
 			contents: 'write',
 			pages: 'write',
@@ -15,24 +15,38 @@ export default createWorkflow(
 			branches: ['next'],
 			paths: ['website/**'],
 		});
+		addTrigger('pull_request', {
+			paths: ['website/**'],
+		});
 		addTrigger('workflow_dispatch');
 		setConcurrency({
 			group: interpolate`${github.workflow}-${github.ref}`,
 			cancelInProgress: true,
 		});
 
-		addJob('deploy', ({ setName, add, run, use }) => {
-			setName('Deploy website');
+		addJob('build', ({ setName, add, run, whenTrigger }) => {
+			setName('Build website');
 			add(setupNode());
 			run('yarn docs:build');
-			use('peaceiris/actions-gh-pages@4f9cc6602d3f66b9c108549d475ec49e8ef4d45e', {
-				with: {
-					github_token: secrets['GITHUB_TOKEN'],
-					publish_dir: './website/build',
-					user_name: 'github-actions[bot]',
-					user_email: '41898282+github-actions[bot]@users.noreply.github.com',
-				},
+			whenTrigger('workflow_dispatch', () => {
+				add(deployWebsite());
+			});
+			whenTrigger('push', () => {
+				add(deployWebsite());
 			});
 		});
 	},
 );
+
+function deployWebsite(): Steps {
+	return ({ use }) => {
+		use('peaceiris/actions-gh-pages@4f9cc6602d3f66b9c108549d475ec49e8ef4d45e', {
+			with: {
+				github_token: secrets['GITHUB_TOKEN'],
+				publish_dir: './website/build',
+				user_name: 'github-actions[bot]',
+				user_email: '41898282+github-actions[bot]@users.noreply.github.com',
+			},
+		});
+	};
+}

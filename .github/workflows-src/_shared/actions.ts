98ef3ea1b3e3c52b208e34e67a42c0aaad5d7e58
Woky <pampu.andrei.social+git@pm.me>
2026-05-12 21:49:04 +0300

    chore: make version lowercase in actions (#444)

--- .github/workflows-src/_shared/actions.ts
+++ .github/workflows-src/_shared/actions.ts
@@ -1,7 +1,7 @@
 export const actions = {
-	checkout: 'actions/checkout@de0fac2e4500dabe0009e67214ff5f5447ce83dd', // V6.0.2
-	setupNode: 'actions/setup-node@48b55a011bda9f5d6aeb4c2d9c7362e8dae4041e', // V6.4.0
-	cache: 'actions/cache@27d5ce7f107fe9357f9df03efb73ab90386fccae', // V5.0.5
+	checkout: 'actions/checkout@1af3b93b6815bc44a9784bd300feb67ff0d1eeb3', // v6.0.0
+	setupNode: 'actions/setup-node@48b55a011bda9f5d6aeb4c2d9c7362e8dae4041e', // v6.4.0
+	cache: 'actions/cache@27d5ce7f107fe9357f9df03efb73ab90386fccae', // v5.0.5
 	createGithubAppToken: 'actions/create-github-app-token@1b10c78c7865c340bc4f6099eb2f838309f1e8c3', // v3.1.1
 	githubScript: 'actions/github-script@3a2844b7e9c422d3c10d287c895573f7108da1b3', // v9.0.0
 	changesets: 'changesets/action@6a0a831ff30acef54f2c6aa1cbbc1096b066edaf', // v1.7.0

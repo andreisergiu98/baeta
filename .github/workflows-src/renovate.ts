0fb038019578ceb7674115af277ba1fe47f48784
Woky <pampu.andrei.social+git@pm.me>
2026-05-12 22:52:38 +0300

    chore: create renovate rule for actions (#445)

--- .github/workflows-src/renovate.ts
+++ .github/workflows-src/renovate.ts
@@ -1,8 +1,11 @@
 import createWorkflow from 'github-actions-workflow-builder';
-import { github, secrets } from 'github-actions-workflow-builder/context';
+import { github, runner } from 'github-actions-workflow-builder/context';
 import { interpolate, neq } from 'github-actions-workflow-builder/lib/expression';
 import { actions } from './_shared/actions.ts';
 import { useBaetaBotToken } from './_shared/bot-token.ts';
+import { setupNode } from './_shared/setup.ts';
+
+const allowedCommands = ['^yarn install$', '^yarn actions:build$'];
 
 export default createWorkflow(
 	({ setWorkflowName, addJob, setConcurrency, addTrigger, setPermissions }) => {
@@ -37,15 +40,7 @@ export default createWorkflow(
 		addJob('Renovate', ({ use, add, when, run }) => {
 			const getToken = add(useBaetaBotToken());
 
-			use('Login to ghcr.io', actions.dockerLogin, {
-				with: {
-					registry: 'ghcr.io',
-					username: github.actor,
-					password: secrets.GITHUB_TOKEN,
-				},
-			});
-
-			use('Checkout', actions.checkout);
+			add(setupNode());
 
 			when(neq(dispatch.inputs.cache, 'disabled'), () => {
 				use('Use Renovate Cache', actions.cache, {
@@ -55,18 +50,24 @@ export default createWorkflow(
 						'restore-keys': 'renovate-cache-',
 					},
 				});
-				run('Fix cache permissions', 'sudo chown -R 12021:0 /tmp/renovate/ || true');
 			});
 
-			use('Run Renovate', actions.renovate, {
+			use('Cache Renovate Install', actions.cache, {
 				with: {
-					token: getToken.outputs.token,
-					configurationFile: '.github/renovate.json',
+					path: '~/.npm',
+					key: interpolate`renovate-npm-cache-${runner.os}`,
+					'restore-keys': 'renovate-npm-cache-',
 				},
+			});
+
+			run('Run Renovate', 'npx --yes renovate', {
 				env: {
+					RENOVATE_TOKEN: getToken.outputs.token,
+					RENOVATE_CONFIG_FILE: '.github/renovate.json',
 					RENOVATE_REPOSITORY_CACHE: dispatch.inputs.cache,
 					RENOVATE_PLATFORM_COMMIT: 'enabled',
 					RENOVATE_REPOSITORIES: github.repository,
+					RENOVATE_ALLOWED_COMMANDS: JSON.stringify(allowedCommands),
 					LOG_LEVEL: dispatch.inputs.logLevel,
 					NODE_OPTIONS: '--max-old-space-size=4096',
 				},

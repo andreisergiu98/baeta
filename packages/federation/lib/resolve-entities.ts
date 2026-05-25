b4f924e8ff28c762ce57997fdafd4cbb19b86674
Andrei Pampu <pampu.andrei@pm.me>
2026-05-25 15:17:19 +0300

    fix: use Map for registering federation entity handlers

--- packages/federation/lib/resolve-entities.ts
+++ packages/federation/lib/resolve-entities.ts
@@ -2,11 +2,6 @@ type EntityRepresentation<T extends string, R extends Record<string, unknown>> =
 	__typename: T;
 } & R;
 
-type EntityHandlerMap<T extends string, Ctx, Info> = Record<
-	T,
-	(representation: EntityRepresentation<T, any>, ctx: Ctx, info: Info) => any
->;
-
 export async function resolveEntities<
 	T extends string,
 	R extends Record<string, unknown>,
@@ -14,12 +9,21 @@ export async function resolveEntities<
 	Info,
 >(
 	representations: Array<EntityRepresentation<T, R>>,
-	entityHandlerMap: EntityHandlerMap<T, Ctx, Info>,
+	entityHandlerMap: Map<
+		string,
+		(representation: EntityRepresentation<T, any>, ctx: Ctx, info: Info) => any
+	>,
 	ctx: Ctx,
 	info: Info,
 ) {
 	const promises = representations.map(async (representation) => {
-		const handler = entityHandlerMap[representation.__typename];
+		const typename = representation.__typename;
+		const handler = entityHandlerMap.get(typename);
+		if (handler == null) {
+			throw new Error(
+				`Entity representation is missing or has invalid __typename: ${String(typename)}`,
+			);
+		}
 		return await handler(representation, ctx, info);
 	});
 	return await Promise.all(promises);

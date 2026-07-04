1a5be91e4181bd9ba0382d163b805e95305d6e53
Woky <pampu.andrei@pm.me>
2026-07-04 21:37:25 +0300

    chore: rename make symbols (#595)

--- packages/core/sdk/symbols.ts
+++ packages/core/sdk/symbols.ts
@@ -1,2 +1,2 @@
-export const makeSymbol: unique symbol = Symbol('make');
-export const makePluginSymbol: unique symbol = Symbol('makePlugin');
+export const makeSymbol: unique symbol = Symbol.for('@baeta/core/make');
+export const makePluginSymbol: unique symbol = Symbol.for('@baeta/core/makePlugin');
